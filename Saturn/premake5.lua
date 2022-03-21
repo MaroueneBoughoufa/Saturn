1a38b034828e6ef74b64d41703808394d10bbfd0
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-03-21 21:40:56 +0100

    Fixed binary directories in premake files

--- Saturn/premake5.lua
+++ Saturn/premake5.lua
@@ -4,8 +4,8 @@ project "Saturn"
 	cppdialect "C++17"
 	staticruntime "on"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("../bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("../bin-int/" .. outputdir .. "/%{prj.name}")
 
 	pchheader "stpch.h"
 	pchsource "src/stpch.cpp"
