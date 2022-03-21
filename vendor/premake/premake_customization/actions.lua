1c55e48f29918c13dc255195844b81543cc3bc6e
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-03-21 21:49:05 +0100

    Update project file path

--- vendor/premake/premake_customization/actions.lua
+++ vendor/premake/premake_customization/actions.lua
@@ -16,8 +16,6 @@ newaction {
 		os.remove("**.vcxproj")
 		os.remove("**.vcxproj.filters")
 		os.remove("**.vcxproj.user")
-		os.rmdir("./Saturn/Saturn")
-		os.rmdir("./Sandbox/Sandbox")
 		print("Done.")
 	end
 }
