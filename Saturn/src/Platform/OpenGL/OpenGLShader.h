1de3847ad3830a0aee405b9c8a6dc4548dbf3c64
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-03-23 20:04:01 +0100

    [Refactor] Refactored shader abstraction

--- Saturn/src/Platform/OpenGL/OpenGLShader.h
+++ Saturn/src/Platform/OpenGL/OpenGLShader.h
@@ -12,7 +12,15 @@ namespace Saturn
 		virtual void Bind() const override;
 		virtual void Unbind() const override;
 
-		virtual void UploadUniformMat4(const std::string& name, const glm::mat4& matrix) override;
+		void UploadUniformInt(const std::string& name, int value);
+
+		void UploadUniformFloat(const std::string& name, float value);
+		void UploadUniformVec2f(const std::string& name, const glm::vec2& vector);
+		void UploadUniformVec3f(const std::string& name, const glm::vec3& vector);
+		void UploadUniformVec4f(const std::string& name, const glm::vec4& vector);
+
+		void UploadUniformMat3(const std::string& name, const glm::mat3& matrix);
+		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
 	private:
 		uint32_t m_RendererID;
 	};
--- Saturn/src/Saturn/Renderer/Shader.h
+++ Saturn/src/Saturn/Renderer/Shader.h
@@ -7,12 +7,10 @@ namespace Saturn
 	class Shader
 	{
 	public:
-		virtual ~Shader() {}
+		virtual ~Shader() = default;
 
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
-
-		virtual void UploadUniformMat4(const std::string& name, const glm::mat4& matrix) = 0;
 
 		static Shader* Create(const std::string vertexSrc, const std::string fragmentSrc);
 	};
