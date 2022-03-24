f461f11e420e16d2233dee36ad399b0641f8e099
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-03-24 14:03:52 +0100

    Added Saturn::Scope and Saturn::Ref definitions

--- Saturn/src/Saturn/Core/Core.h
+++ Saturn/src/Saturn/Core/Core.h
@@ -34,3 +34,13 @@
 
 #define BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
 #define BIT(x) (1 << x)
+
+
+namespace Saturn
+{
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+}
