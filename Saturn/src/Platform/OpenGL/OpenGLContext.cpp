31fc4b17e438e3fa6062018087a79e29d66c5c3c
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-04-18 10:39:21 +0000

    Added check for OpenGL version

--- Saturn/src/Platform/OpenGL/OpenGLContext.cpp
+++ Saturn/src/Platform/OpenGL/OpenGLContext.cpp
@@ -18,7 +18,16 @@ namespace Saturn
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		ST_CORE_ASSERT(status, "Failed to initialize Glad!");
 
-		ST_CORE_INFO("OpenGL Info:\tGPU: {0}\tVersion: {1}", glGetString(GL_RENDERER), glGetString(GL_VERSION));
+		ST_CORE_INFO("OpenGL Info: \tGPU: {0}\tVersion: {1}", glGetString(GL_RENDERER), glGetString(GL_VERSION));
+
+	#ifdef ST_ENABLE_ASSERT
+		int versionMajor;
+		int versionMinor;
+		glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+		glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+		ST_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "Saturn requires atleast OpenGL 4.5!");
+	#endif
 	}
 
 	void OpenGLContext::SwapBuffers()
