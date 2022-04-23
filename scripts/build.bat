443837ff05b2ad2008816b49f66e5f8893e5061f
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-04-23 18:37:23 +0000

    'build.bat' can now be called from any folder

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-pushd ..
+pushd %~dp0\..\
 
 if not defined DevEnvDir (
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
@@ -13,7 +13,7 @@ if "~%1" == "Release" ( set config="Release" )
 if "~%1" == "Dist" ( set config="Dist" )
 
 set solutionFile="SaturnEngine.sln"
-msbuild /t:Build /p:Configuration=%config% /p:Platform=x64 %solutionFile%
+msbuild -m /t:Build /p:Configuration=%config% /p:Platform=x64 %solutionFile%
 
 popd
 
