54a25dd7b802957392b0667bb4a0732ef3ac5e76
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-05-12 20:41:34 +0100

    Update python script

--- scripts/Setup.py
+++ scripts/Setup.py
@@ -18,7 +18,11 @@ subprocess.call(["git", "submodule", "update", "--init", "--recursive"])
 if (premakeInstalled):
     if platform.system() == "Windows":
         print("\nRunning Premake")
-        subprocess.call([os.path.abspath("./scripts/Win-GenProject-vs2019.bat"), "nopause"])
+        subprocess.call([os.path.abspath("./premake.bat"),  "vs2019", "nopause"])
+        print("\nSetup completed!")
+    if platform.system() == "Linux":
+        print("\nRunning Premake")
+        subprocess.call([os.path.abspath("./premake.bat "), "gmake2", "nopause"])
         print("\nSetup completed!")
 else:
     print("Saturn requires Premake to generate project files.")
