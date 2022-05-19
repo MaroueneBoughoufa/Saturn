fd033290b29d5f74a6f32e871409c97f0e673a6e
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-05-19 20:12:46 +0100

    Maintenance stuff

---
+++ Sandbox/build.bat
@@ -0,0 +1,20 @@
+@echo off
+
+if not defined DevEnvDir (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+)
+
+set config="Dist"
+
+set project="Sandbox.vcxproj"
+
+if "%1" == "Debug" ( set config="Debug" )
+if "%1" == "Release" ( set config="Release" )
+if "%1" == "Dist" ( set config="Dist" )
+
+if not exist %project% (
+        call premake.bat vs2019
+)
+
+
+msbuild %project% /t:Build /p:Configuration=%config% /p:Platform=x64
