ed3944881fa95de78def9997874bd207b36ddfd3
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-05-27 21:23:13 +0100

    Readded Core.h (cause i'm stupid)

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,8 +1,172 @@
 #include <Saturn.h>
 #include <Saturn/Core/EntryPoint.h>
 
+<<<<<<< Updated upstream
 //#include "ExampleLayer.h"
 #include "Sandbox2D.h"
+=======
+#include <glm/gtc/matrix_transform.hpp>
+
+class GameLayer : public Saturn::Layer
+{
+public:
+	GameLayer()
+		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f)
+	{
+		Saturn::BufferLayout layout = {
+			{ Saturn::ShaderDataType::Vec3f, "a_Position" },
+			{ Saturn::ShaderDataType::Vec4f, "a_Color" }
+		};
+
+		m_SquareVA.reset(Saturn::VertexArray::Create());
+		
+		float squareVertices[] = {
+			// Position           // Color
+			0.5f,  0.5f,  0.0f,   0.8f, 0.2f, 0.8f, 1.0f,  // top right
+			0.5f, -0.5f,  0.0f,   0.2f, 0.3f, 0.8f, 1.0f,  // bottom right
+		   -0.5f, -0.5f,  0.0f,   0.8f, 0.8f, 0.2f, 1.0f,  // bottom left
+		   -0.5f,  0.5f,  0.0f,   0.3f, 0.2f, 0.8f, 1.0f   // top left
+		};
+
+		std::shared_ptr<Saturn::VertexBuffer> squareVertexBuffer;
+		squareVertexBuffer.reset(Saturn::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
+
+
+		squareVertexBuffer->SetLayout(layout);
+		m_SquareVA->AddVertexBuffer(squareVertexBuffer);
+
+		uint32_t squareIndices[] = {
+			0, 1, 3,
+			1, 2, 3
+		};
+
+		std::shared_ptr<Saturn::IndexBuffer> squareIndexBuffer;
+
+		squareIndexBuffer.reset(Saturn::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
+		m_SquareVA->SetIndexBuffer(squareIndexBuffer);
+
+		m_VertexArray.reset(Saturn::VertexArray::Create());
+
+		float vertices[] = {
+			 0.0f,  0.5f,  0.0f,   1.0f, 0.0f, 0.0f, 1.0f,
+			 0.5f, -0.5f,  0.0f,   1.0f, 0.0f, 0.0f, 1.0f,
+			-0.5f, -0.5f,  0.0f,   1.0f, 0.0f, 0.0f, 1.0f
+		};
+
+		std::shared_ptr<Saturn::VertexBuffer> vertexBuffer;
+		vertexBuffer.reset(Saturn::VertexBuffer::Create(vertices, sizeof(vertices)));
+
+		vertexBuffer->SetLayout(layout);
+		m_VertexArray->AddVertexBuffer(vertexBuffer);
+
+		uint32_t indices[] = {
+			1, 2, 3
+		};
+
+		std::shared_ptr<Saturn::IndexBuffer> indexBuffer;
+		indexBuffer.reset(Saturn::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
+
+		m_VertexArray->SetIndexBuffer(indexBuffer);
+
+		std::string vertexSrc = R"(
+			#version 330 core
+
+			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+
+			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
+
+			out vec3 v_Position;
+			out vec4 v_Color;
+
+			void main()
+			{
+				v_Position = a_Position;
+				v_Color = a_Color;
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
+			}
+		)";
+
+		std::string fragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+			in vec4 v_Color;
+			
+			void main()
+			{
+				color = v_Color;
+			}
+		)";
+
+		m_Shader.reset(Saturn::Shader::Create(vertexSrc, fragmentSrc));
+	}
+
+	void OnUpdate(Saturn::Timestep ts) override
+	{
+		if (Saturn::Input::IsKeyPressed(KEY_LEFT))
+			m_CameraPosition.x -= m_CameraSpeed * ts;
+		else if (Saturn::Input::IsKeyPressed(KEY_RIGHT))
+			m_CameraPosition.x += m_CameraSpeed * ts;
+		
+		if (Saturn::Input::IsKeyPressed(KEY_UP))
+			m_CameraPosition.y += m_CameraSpeed * ts;
+		else if (Saturn::Input::IsKeyPressed(KEY_DOWN))
+			m_CameraPosition.y -= m_CameraSpeed * ts;
+
+		if (Saturn::Input::IsKeyPressed(KEY_Q))
+			m_CameraRotation += m_CameraRotationSpeed * ts;
+		else if (Saturn::Input::IsKeyPressed(KEY_E))
+			m_CameraRotation -= m_CameraRotationSpeed * ts;
+
+		Saturn::RenderCommand::SetClearColor(glm::vec4(0.2f, 0.2f, 0.2f, 1));
+		Saturn::RenderCommand::Clear();
+
+		m_Camera.SetPosition(m_CameraPosition);
+		m_Camera.SetRotation(m_CameraRotation);
+
+		Saturn::Renderer::BeginScene(m_Camera);
+		Saturn::RenderCommand::SetWireframeMode(false);
+
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 5; y++)
+		{
+			for (int x = 0; x < 5; x++)
+			{
+				glm::vec3 pos(x * 0.25f, y * 0.25f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+
+				Saturn::Renderer::Submit(m_Shader, m_SquareVA, transform);
+			}
+		}
+
+		Saturn::Renderer::Submit(m_Shader, m_VertexArray);
+		Saturn::Renderer::EndScene();
+	}
+
+	void OnEvent(Saturn::Event& event) override
+	{
+	}
+
+	void OnImGuiRender() override
+	{
+	}
+private:
+	std::shared_ptr<Saturn::VertexArray> m_VertexArray;
+	std::shared_ptr<Saturn::VertexArray> m_SquareVA;
+	std::shared_ptr<Saturn::Shader> m_Shader;
+
+	Saturn::OrthoCamera m_Camera;
+	glm::vec3 m_CameraPosition;
+	float m_CameraSpeed = 3.0f;
+	float m_CameraRotation = 0.0f;
+	float m_CameraRotationSpeed = 60.0f;
+};
+>>>>>>> Stashed changes
 
 class Sandbox : public Saturn::Application
 {
---
+++ Saturn/src/Saturn/Core/Core.h
@@ -0,0 +1,101 @@
+#pragma once
+
+#include <memory>
+
+// Platform Detection
+#ifdef _WIN32
+	#ifdef _WIN64
+		#define ST_PLATFORM_WINDOWS
+	#else
+		#error "x86 builds are not supported"
+	#endif
+#elif defined(__APPLE__) || defined(__MACH__)
+	#include <TargetConditionals.h>
+	/* TARGET_OS_MAC exists on all the platforms
+	* so we must check all of them (in this order)
+	* to ensure that we're running on MAC
+	* and not some other Apple platform */
+	#if TARGET_IPHONE_SIMULATOR == 1
+		#error "IOS simulator is not supported!"
+	#elif TARGET_OS_IPHONE == 1
+		#define ST_PLATFORM_IOS
+		#error "IOS is not supported!"
+	#elif TARGET_OS_MAC == 1
+		#define ST_PLATFORM_MACOS
+		#error "MacOS is not supported!"
+	#else
+		#error "Unknown Apple platform!"
+	#endif
+/* We also have to check __ANDROID__ before __linux__
+ * since android is based on the linux kernel
+ * it has __linux__ defined */
+#elif defined (__ANDROID__)
+	#define ST_PLATFORM_ANDROID
+	#error "Android is not supported!"
+#elif defined (__linux__)
+	#define ST_PLATFORM_LINUX
+	#error "Linux is not supported!"
+#endif
+// End Platform detection
+
+
+/* DLL support */
+#ifdef ST_DYNAMIC_LINK
+	#ifdef _MSC_VER
+		#ifdef ST_BUILD_DLL
+			#define SATURN_API __declspec(dllexport)
+		#else
+			#define SATURN_API __declspec(dllimport)
+		#endif
+	#elif defined (__GNUC__)
+		#define SATURN_API __attribute__((visibility("default")))
+	#endif
+#else
+	#define SATURN_API
+#endif
+/* End DLL support */
+
+
+#ifdef ST_DEBUG
+	#define ST_ENABLE_ASSERT
+
+	#if defined(_MSC_VER) && defined (ST_PLATFORM_WINDOWS)
+		#define ST_DEBUG_BREAK() __debugbreak()
+	#else
+		#include <signal.h>
+		#define ST_DEBUG_BREAK() raise(SIGTRAP)
+	#endif
+#endif /* ST_DEBUG */
+
+#ifdef ST_ENABLE_ASSERT
+	#define ST_ASSERT(x, ...) { if(!(x)) { ST_ERROR("Assertion Failed: {0}", __VA_ARGS__); ST_DEBUG_BREAK(); } }
+	#define ST_CORE_ASSERT(x, ...) { if(!(x)) { ST_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); ST_DEBUG_BREAK(); } }
+#else
+	#define ST_ASSERT(x, ...)
+	#define ST_CORE_ASSERT(x, ...)
+#endif /* ST_ENABLE_ASSERT */
+
+#define BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define BIT(x) (1 << x)
+
+
+namespace Saturn
+{
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template<typename T, typename ... Args>
+	constexpr Scope<T> CreateScope(Args&& ... args)
+	{
+		return std::make_unique<T>(std::forward<Args>(args)...);
+	}
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+
+	template<typename T, typename ... Args>
+	constexpr Ref<T> CreateRef(Args&& ... args)
+	{
+		return std::make_shared<T>(std::forward<Args>(args)...);
+	}
+}
